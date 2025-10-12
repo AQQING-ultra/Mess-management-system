a0bd8df4504f02c42f4bfb075cb3beb59f6707bd
M AWAIS AKRAM <mawaisqurnimakram@gmail.com>
2025-10-12 12:08:44 -0700

    Add files via upload

---
+++ Bs AI-A- 21.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int inven[5] = {9, 5, 15, 60, 8};
+    int* awa = inven;
+    cout << "Inventory before update:"<<endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Product " << i << " = " << *(awa + i) << endl;
+    }
+    int id, change;
+    cout << "Enter product ID from 0 to 4: ";
+    cin >> id;
+    if (id < 0 || id > 4) 
+    {
+        cout << "Invalid ID";
+    }
+    cout << "Enter quantity to add or remove: ";
+    cin >> change;
+    int& Stock = inven[id];
+    if (Stock + change < 0) {
+        cout << "Not enough stock"<<endl;
+    }
+    Stock = Stock + change;
+    cout << "Inventory after update:"
+    for (int i = 0; i < 5; i++) {
+        cout << "Product " << i << " = " << *(awa + i) << endl;
+    }
+    return 0;
+}
